6f21d39fdd96ed17e59f49d33520327ca5854385
Jason Loomis <jason@jrloomis.com>
2019-08-22 01:30:03 -0400

    simple layout

--- src/Components/SinglePost.js
+++ src/Components/SinglePost.js
@@ -1,7 +1,48 @@
 import React from "react";
+import Moment from "react-moment";
+import { Card, Image } from "semantic-ui-react";
 
-const SinglePost = () => {
-  return <div />;
+const SinglePost = ({ post }) => {
+  const formatDate = post.wedding_date;
+
+  return (
+    <Card centered raised>
+      <Image src={post.item_photo} alt={post.couple_name} />
+      <Card.Content>
+        <Card.Header as="h2">{post.couple_name}</Card.Header>
+        <Card.Meta>
+          <Moment format="MMMM DD, YYYY">{formatDate}</Moment>
+        </Card.Meta>
+        <Card.Meta>{post.wedding_location}</Card.Meta>
+        <Card.Meta>{post.wedding_theme}</Card.Meta>
+      </Card.Content>
+      <Card.Content extra>
+        <Card.Meta>Photography by {post.wedding_photographer}</Card.Meta>
+      </Card.Content>
+      <Card.Content extra>
+        <Card.Description>User ID is always {post.user_id}?</Card.Description>
+      </Card.Content>
+    </Card>
+  );
 };
 
 export default SinglePost;
+
+{
+  /* <div>{post.couple_name}</div> */
+}
+{
+  /* <img src={post.item_photo} /> */
+}
+{
+  /* <div>{post.wedding_date}</div> */
+}
+{
+  /* <div>{post.wedding_location}</div> */
+}
+{
+  /* <div>{post.wedding_photographer}</div> */
+}
+{
+  /* <div>{post.wedding_theme}</div> */
+}
