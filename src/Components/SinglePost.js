d91792104ad34d071142a368819442d404df661e
Jason Loomis <jason@jrloomis.com>
2019-08-22 01:43:17 -0400

    didn't need the variable

--- src/Components/SinglePost.js
+++ src/Components/SinglePost.js
@@ -3,15 +3,13 @@ import Moment from "react-moment";
 import { Card, Image } from "semantic-ui-react";
 
 const SinglePost = ({ post }) => {
-  const formatDate = post.wedding_date;
-
   return (
     <Card centered raised>
       <Image src={post.item_photo} alt={post.couple_name} />
       <Card.Content>
         <Card.Header as="h2">{post.couple_name}</Card.Header>
         <Card.Meta>
-          <Moment format="MMMM DD, YYYY">{formatDate}</Moment>
+          <Moment format="MMMM DD, YYYY">{post.wedding_date}</Moment>
         </Card.Meta>
         <Card.Meta>{post.wedding_location}</Card.Meta>
         <Card.Meta>{post.wedding_theme}</Card.Meta>
