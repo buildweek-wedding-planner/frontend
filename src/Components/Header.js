f669b67ea9ddeca739c5a0edb5587f72a26dbf7e
Jason Loomis <jason@jrloomis.com>
2019-08-24 13:56:53 -0400

    this will include Onboarding, Login, logo, etc

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Onboarding from "./Onboarding";
+import Login from "./Login";
+
+const Header = () => {
+  return (
+    <div>
+      <Onboarding />
+      <Login />
+    </div>
+  );
+};
+
+export default Header;
