21343d3a63125affc32c09e87f7cdb6aaebaa30e
Jason Loomis <jason@jrloomis.com>
2019-08-24 13:56:06 -0400

    added HomePage route

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import HomePage from "./HomePage";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
 const Router = () => {
   return (
     <div>
+      <Route exact path="/" component={HomePage} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/dashboard" component={Dashboard} />
     </div>
