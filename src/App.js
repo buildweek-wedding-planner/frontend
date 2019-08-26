2eb1b8bb4e9041987d7709e2fb8e2ca2cad267f6
Denvinn Magsino <denvinnmagsino@Denvinns-MacBook-Air.local>
2019-08-26 13:38:49 -0700

    finsihed formik

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from "react";
 import "./App.scss";
 
+
 import HomePage from "./Components/HomePage";
+<<<<<<< HEAD
 // import Onboarding from "./Components/Onboarding";
 import SignUpModal from "./Components/SignUpModal";
+=======
+import Onboarding from "./Components/Onboarding";
+import Router from "./Components/Router";
+
+>>>>>>> 28e046a4230f8225c67f120abf7f1c0f9da9d632
 
 function App() {
   return (
     <div>
+<<<<<<< HEAD
       <SignUpModal />
       {/* <Onboarding /> */}
+=======
+
+      <Onboarding />
+>>>>>>> 28e046a4230f8225c67f120abf7f1c0f9da9d632
       <HomePage />
+
+      <Router />
+
     </div>
   );
 }
