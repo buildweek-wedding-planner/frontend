596404a2b13c61a5f27a6cca9d5513c696b20522
Jason Loomis <jason@jrloomis.com>
2019-08-26 23:04:17 -0400

    login works - when using reqres.in

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,57 +1,63 @@
-import React from 'react'
-import axios from 'axios'
-import { Form, Formik, Field, withFormik } from 'formik'
-import * as Yup from 'yup'
-import { Card, Button, Modal } from 'semantic-ui-react'
+import React from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import { Form, Formik, Field, withFormik } from "formik";
+import * as Yup from "yup";
+import { Card, Button, Modal } from "semantic-ui-react";
 
 const Login = props => {
-  const validationSchema = Yup.object().shape({
-    email: Yup.string(),
-    password: Yup.string()
-  })
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
-    <Modal
-      basic
-      centered
-      closeIcon
-      trigger={<Button content={'Sign In'} />}>
+    <Modal basic centered closeIcon trigger={<Button content={"Sign In"} />}>
       <Modal.Content>
         <Card raised centered>
           <Card.Content>
-            <Card.Header as="h2" textAlign={"center"} >Login</Card.Header>
-            <Card.Description textAlign={'center'} >
-              <Formik
-                initialValues={{ email: "", password: "" }}
-                validationSchema={validationSchema}
-                onSubmit={(values, { resetForm, setStatus, history }) => {
-                  axios
-                    .post("https://reqres.in/api/users", values)
-                    .then(resolve => {
-                      console.log('login resolve > ', resolve)
-                      console.log('login props > ', props)
-                      console.log('history > ', props.history)
-                      props.history.push('/dashboard')
-                    })
-                    .catch(error => {
-                      console.log('login error > ', error)
-                    })
-                }}>
-                {({ isSubmitting, errors, touched }) => (
-                  <Form className='loginForm'>
-                    <Field component="input" type="text" name="email" placeholder="email" className="loginForm__field" />
-                    <Field component="input" type="text" name="password" placeholder="password" className="loginForm__field" />
-                    <Button type="submit" content="Submit" />
-                  </Form>
-                )}
-              </Formik>
+            <Card.Header as="h2" textAlign={"center"}>
+              Login
+            </Card.Header>
+            <Card.Description textAlign={"center"}>
+              <Form className="loginForm">
+                <Field component="input" type="text" name="email" placeholder="Email" className="loginForm__field" />
+                <Field component="input" type="password" name="password" placeholder="Password" className="loginForm__field" />
+                <Button type="submit" content="Submit" />
+              </Form>
             </Card.Description>
           </Card.Content>
         </Card>
       </Modal.Content>
     </Modal>
   );
+};
 
-}
+export default withFormik({
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string(),
+    password: Yup.string()
+  }),
+  handleSubmit(values, { resetForm, formikBag }) {
+    axios
+      .post("https://reqres.in/api/login", values)
+      .then(resolve => {
+        console.log("login resolve > ", resolve);
+        localStorage.setItem("token", resolve.data.token);
+        formikBag.props.history.push("/dashboard");
+      })
+      .catch(error => {
+        console.log("login error > ", error);
+        resetForm();
+      });
+  }
+})(Login);
 
-export default Login
+// https://lambda-wedding-planner.herokuapp.com/api/auth/login
