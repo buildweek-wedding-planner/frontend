163ff37dd574eec2ff090eba7c663fb77df12a98
Jason Loomis <jason@jrloomis.com>
2019-08-28 22:11:37 -0400

    returns different nav menu - logged in/not logged in

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import PostPage from "./PostPage";
 import SignUpModal from "./SignUpModal";
 import Login from "./Login";
 
 const Navigation = ({ history }) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    return <PostPage />;
+  }
   return (
     <>
       <SignUpModal />
