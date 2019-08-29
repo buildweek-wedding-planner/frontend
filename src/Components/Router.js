58f80684663e38b8b9c0179ebfc7899e129eceab
Jason Loomis <jason@jrloomis.com>
2019-08-29 01:42:21 -0400

    protected route to PostPage

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,6 +4,7 @@ import { PrivateRoute } from "./PrivateRoute";
 import HomePage from "./HomePage";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
+import PostPage from "./PostPage";
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
       <Route exact path="/" component={HomePage} />
       <Route exact path="/login" component={Login} />
       <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      <PrivateRoute exact path="/newpost" component={PostPage} />
     </div>
   );
 };
