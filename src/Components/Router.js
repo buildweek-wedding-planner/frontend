2a7ed418c90dac19fb6fe9c7065ffe0f48097c14
Jason Loomis <jason@jrloomis.com>
2019-08-28 21:39:05 -0400

    changed how protected routes are handled

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import { PrivateRoute } from "./PrivateRoute";
 import HomePage from "./HomePage";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
@@ -9,17 +10,7 @@ const Router = () => {
     <div>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/login" component={Login} />
-      <Route
-        exact
-        path="/dashboard"
-        render={props => {
-          const token = localStorage.getItem("token");
-          if (!token) {
-            return <Redirect to="/" />;
-          }
-          return <Dashboard {...props} />;
-        }}
-      />
+      <PrivateRoute exact path="/dashboard" component={Dashboard} />
     </div>
   );
 };
