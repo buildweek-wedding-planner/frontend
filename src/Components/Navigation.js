becde8e16c6ad502a93e68492cb79e540cdad968
Jason Loomis <jason@jrloomis.com>
2019-08-31 10:42:47 -0400

    clean up

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavLink, withRouter } from "react-router-dom";
 import { Button } from "semantic-ui-react";
-import PostPage from "./PostPage";
 import Onboarding from "./Onboarding";
 import Login from "./Login";
 
