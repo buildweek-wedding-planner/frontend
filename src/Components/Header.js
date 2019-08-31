4720a46720c4d67215a145d79e0991a164350967
Jason Loomis <jason@jrloomis.com>
2019-08-31 13:03:09 -0400

    moved search to Header.js for styling purposes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,14 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import Navigation from "./Navigation";
 import { Image } from "semantic-ui-react";
+import Search from "./Search";
 
 const Header = ({ history }) => {
   return (
     <div className="pageHeader">
       <Image src={require("../img/logo.png")} size="small" />
       <Navigation />
+      <Search />
     </div>
   );
 };
