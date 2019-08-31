5bcaedd748b627909b2f1d2a25734448c93efaf7
Jason Loomis <jason@jrloomis.com>
2019-08-31 13:00:07 -0400

    added logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import Navigation from "./Navigation";
+import { Image } from "semantic-ui-react";
 
 const Header = ({ history }) => {
   return (
     <div className="pageHeader">
+      <Image src={require("../img/logo.png")} size="small" />
       <Navigation />
     </div>
   );
